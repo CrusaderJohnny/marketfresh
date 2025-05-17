7a25c6675e5999751bbf3d407bb52b2fc7607909
CrusaderJohnny <17ark.leonardis@gmail.com>
2025-05-17 12:09:49 -0600

    start list component

--- app/marketmap/marketList.js
+++ app/marketmap/marketList.js
@@ -1,5 +1,9 @@
 export default function MarketList() {
     return(
-        <p>This will hold the map and the market component allowing it to list each visible market below or to the side of the map</p>
+        <>
+            <div>
+                <Market/>
+            </div>
+        </>
     )
 }
