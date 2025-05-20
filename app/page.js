b28c602680fae22da2ac6b7b882281851a9e7656
CrusaderJohnny <17ark.leonardis@gmail.com>
2025-05-20 11:56:52 -0600

    started implementation of mantine, changed layout.js for proper load config for mantine

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <p>Hello World, What the....</p>
-        <Button component={Link} href="/marketmap"/>
+        <Button component={Link} href="/marketmap" className="w-50 h-15 bg-white text-black border-2 flex justify-center items-center">Mantine buttons</Button>
         <button className="w-50 h-15 bg-white text-black border-2" onClick={() => router.push("marketmap")}>Tika taka toey</button>
         <button className="w-50 h-15 bg-white text-black border-2" onClick={() => router.push("map")}>Map button</button>
         <button className="w-50 h-15 bg-white text-black border-2" onClick={() => router.push("stickyHeaderAndCompTest")}>Header</button>
