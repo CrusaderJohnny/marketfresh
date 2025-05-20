7681e75b17bffda2cd5b9c431dfe93f28973776e
CrusaderJohnny <17ark.leonardis@gmail.com>
2025-05-20 08:42:15 -0600

    import fix

--- app/marketmap/marketList.js
+++ app/marketmap/marketList.js
@@ -1,3 +1,5 @@
+import Market from "./market";
+
 export default function MarketList() {
     return(
         <p>This will hold the map and the market component allowing it to list each visible market below or to the side of the map</p>
