362ac50262444762157b90a1151011d2c01d1fbe
Daniel A. <danielasefa975@gmail.com>
2025-05-22 23:38:24 -0600

    Replaced Header with Mantine
    Deprecated old TailwindCSS header & comp test page. Replaced with versions using Matine

--- app/_components/nav/Nav.js
+++ app/_components/nav/Nav.js
@@ -1,3 +1,5 @@
+// Deprecated
+
 'use client';
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
---
+++ app/_components/nav/NavMT.js
@@ -0,0 +1,111 @@
+/*
+Author: Daniel Asefa
+website header using Mantine
+
+References:
+
+
+
+*/
+"use client"
+import { AppShell, useMantineTheme, Button, Group, BackgroundImage, Title, Image } from '@mantine/core';
+// import TestLogo from '../../../public/images/Market-Image-PlaceHolder.jpeg'
+
+export default function NavMT() {
+
+    const theme = useMantineTheme()
+
+    return (
+        <AppShell
+        >
+            <AppShell.Header>
+                {/* main div containing the header */}
+                <BackgroundImage src="https://letspasta.com/wp-content/uploads/2022/08/Alberta-farming.jpg"
+                    style={{
+                        backgroundColor: theme.colors.red[6], // Use if color preferred over image (team not decided)
+                        height: '100%',
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        padding: '0 3vh',
+                    }}
+                >
+                    {/* Header right side */}
+                    <div
+                        style={{width:'25%',
+                            display: 'flex',
+                            justifyContent: 'space-between' }}
+                    >
+                        <Image
+                            src='https://media.istockphoto.com/id/1170724138/vector/farmers-market-hand-drawn-lettering.jpg?s=1024x1024&w=is&k=20&c=EI--kDMvBM9pvC9jFJcaoepQHcDbTxp-De6fgIVqy_8='
+                            h={80}
+                            w='auto'
+                            fit='contain'
+                            radius='md'
+                        />
+
+                        <div>
+                            <Title order={1} c='white'>
+                                Market
+                                <Title pl='5vw'>
+                                    Fresh
+                                </Title>
+                            </Title>
+                        </div>
+                    </div>
+
+                    {/* Header let side where nav to sections of the page */}
+                    <Group spacing='md' >
+                        <Button
+                            component='a'
+                            href='#section1'
+                            variant='subtle'
+                            color='white'
+                            size='lg'
+                        >
+                            Markets
+                        </Button>
+                        <Button
+                            component='a'
+                            href='#section2'
+                            variant='subtle'
+                            color='white'
+                            size='lg'
+                        >
+                            Categories
+                        </Button>
+                        <Button
+                            component='a'
+                            href='#section3'
+                            variant='subtle'
+                            color='white'
+                            size='lg'
+                        >
+                            News
+                        </Button>
+                        <Button
+                            component='a'
+                            href='#section4'
+                            variant='subtle'
+                            color='white'
+                            size='lg'
+                        >
+                            Vendors
+                        </Button>
+                        <Button
+                            component='a'
+                            href='#section5'
+                            variant='subtle'
+                            color='white'
+                            size='lg'
+                        >
+                            Contact
+                        </Button>
+                    </Group>
+                </BackgroundImage>
+
+            </AppShell.Header>
+        </AppShell>
+
+    );
+}
